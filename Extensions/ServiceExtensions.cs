0714411b18f43d0b9e9c8802c9af165bccfefae6
Hung Dang Hong <dhh@aod.vn>
2023-05-26 16:56:04 +0700

    first commit

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace ShopOnline.IDP.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddConfigurationIdentityServer(this IServiceCollection services)
+        {
+            services.AddIdentityServer(options =>
+            {
+                // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
+                options.EmitStaticAudienceClaim = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+            })
+            .AddDeveloperSigningCredential()
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryClients(Config.Clients)
+            .AddInMemoryApiResources(Config.ApiResource)
+            .AddTestUsers(TestUsers.Users)
+            ;
+            return services;
+        }
+
+        public static IServiceCollection AddConfigurationCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+            return services;
+        }
+    }
+}
